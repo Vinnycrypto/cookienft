a02b8af0b8e43b4e652d826215ac52e414368778
Vinnycrypto <164323898+Vinnycrypto@users.noreply.github.com>
2024-07-29 16:51:23 +0530

    Add files via upload

---
+++ hardhat.config.ts
@@ -0,0 +1,12 @@
+"REST_OF_YOUR_CONFIG_GOES_HERE",
+module.exports = {
+    "REST_OF_YOUR_CONFIG_GOES_HERE",
+    networks: {
+        citrea: {
+            url: "https://rpc.devnet.citrea.xyz",
+            chainId: 62298,
+            accounts: ["Y2375f4f68b30f61f76e93632718e6bd406377ed7e968e8aec2e2e23495088969"],
+        },
+    },
+    "REST_OF_YOUR_CONFIG_GOES_HERE",
+};
